3448a6b797ec422cad3f5d1fbe4a3ee574b2b874
Jen <jenniferanndcb@gmail.com>
2019-05-01 08:12:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,3 +9,15 @@ end
 def first_element (arr)
   return arr [0]
 end
+
+def third_element (arr)
+  return arr [2]
+end
+
+def last_element (arr)
+  return arr [-1]
+end
+
+def first_element_with_array_methods (arr)
+  return arr.first
+end
